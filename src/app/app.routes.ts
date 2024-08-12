3da4549d8167453dab2273e9953aa5d13ba8c124
Алина Шахабова <alina.shakhabova@yandex.ru>
2024-08-12 15:18:00 +0300

    login component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,18 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { LoginComponent } from './components/sign-in/login/login.component';
 
 export const routes: Routes = [
-  { path: 'sign-in', component: SignInComponent, title: 'Sign in'}
+  { 
+    path: 'sign-in', 
+    component: SignInComponent, 
+    title: 'Sign in', 
+    children: [
+      { path: 'login', component: LoginComponent }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '/sign-in/login'
+  }
 ];
--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
