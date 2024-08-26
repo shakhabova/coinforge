e22f9038a36844caf30b9d4987d91aa13b18b89c
Алина Шахабова <alina.shakhabova@yandex.ru>
2024-08-26 16:20:29 +0300

    sign-up component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { LoginComponent } from './components/sign-in/login/login.component';
+import { SignUpComponent } from './components/sign-in/login/sign-up/sign-up.component';
 
 export const routes: Routes = [
   { 
@@ -8,7 +9,8 @@ export const routes: Routes = [
     component: SignInComponent, 
     title: 'Sign in', 
     children: [
-      { path: 'login', component: LoginComponent }
+      { path: 'login', component: LoginComponent },
+      { path: 'sign-up', component: SignUpComponent}
     ]
   },
   {
---
+++ src/app/components/sign-in/opt-code/opt-code.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-opt-code',
+  standalone: true,
+  imports: [],
+  templateUrl: './opt-code.component.html',
+  styleUrl: './opt-code.component.css'
+})
+export class OptCodeComponent {
+
+}
