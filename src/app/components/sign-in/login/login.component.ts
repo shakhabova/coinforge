4a2f3675da9f19fd49a70af972f7fcda1c57dbea
Алина Шахабова <alina.shakhabova@yandex.ru>
2024-08-27 15:00:01 +0300

    work in progress

--- src/app/components/sign-in/login/login.component.ts
+++ src/app/components/sign-in/login/login.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogModule,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { OptCodeComponent } from '../opt-code/opt-code.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  private dialog = inject(MatDialog);
 
+  signIn() {
+    this.dialog.open(OptCodeComponent);
+  }
 }
--- src/app/components/sign-in/opt-code/opt-code.component.ts
+++ src/app/components/sign-in/opt-code/opt-code.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-opt-code',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './opt-code.component.css'
 })
 export class OptCodeComponent {
+  private dialogRef = inject(MatDialogRef);
 
+  onClose() {
+    this.dialogRef.close();
+  }
 }
