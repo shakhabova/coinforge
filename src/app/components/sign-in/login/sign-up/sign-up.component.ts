530cbd5753ba3d40ce59826b44d9617c45f0715e
Алина Шахабова <alina.shakhabova@yandex.ru>
2024-09-05 15:51:56 +0300

    sign up in progress

--- src/app/components/sign-in/login/sign-up/sign-up.component.ts
+++ src/app/components/sign-in/login/sign-up/sign-up.component.ts
@@ -1,14 +1,23 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, Signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { passwordEqualsValidator } from '../../../../utils/validators';
 import { CommonModule } from '@angular/common';
 import { TuiInputModule, TuiSelectModule, TuiTextfieldControllerModule } from '@taiga-ui/legacy';
-import { TuiDataList, TuiLabel, TuiScrollable, TuiScrollbar, TuiTextfield } from '@taiga-ui/core';
-import { TuiDataListWrapper } from '@taiga-ui/kit';
+import { TuiDataList, TuiError, TuiIcon, TuiLabel, TuiScrollable, TuiScrollbar, TuiTextfield } from '@taiga-ui/core';
+import { TUI_VALIDATION_ERRORS, TuiDataListWrapper, TuiFieldErrorPipe, TuiInputPassword } from '@taiga-ui/kit';
 import { COUNTRY_CODES } from '../../../../utils/constants';
 import { CdkFixedSizeVirtualScroll, CdkVirtualForOf, CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 type Gender = 'MALE' | 'FEMALE';
+
+interface PasswordCriteriaModel {
+  length: boolean;
+  uppercase: boolean;
+  lowercase: boolean;
+  number: boolean;
+  char: boolean;
+}
 
 @Component({
   selector: 'app-sign-up',
@@ -28,9 +37,21 @@ type Gender = 'MALE' | 'FEMALE';
     CdkFixedSizeVirtualScroll,
     CdkVirtualForOf,
     TuiScrollbar,
+    TuiError,
+    TuiFieldErrorPipe,
+    TuiInputPassword,
+    TuiIcon,
   ],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.less'
+  styleUrl: './sign-up.component.less',
+  providers: [
+    {
+      provide: TUI_VALIDATION_ERRORS,
+      useValue: {
+        email: 'Please enter a valid email, e.g., name@example.com'
+      }
+    }
+  ]
 })
 export class SignUpComponent {
   private fb = inject(FormBuilder);
@@ -41,7 +62,7 @@ export class SignUpComponent {
     email: ['', [Validators.required, Validators.email]],
     phoneCountryCode: ['', [Validators.required]],
     phoneNumber: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(13)]],
-    password: ['', [Validators.required, Validators.minLength(6), Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$/)]],
+    password: ['', [Validators.required, Validators.minLength(6), Validators.pattern(/^(?=.*?[A-ZА-Я])(?=.*?[a-zа-я])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$/)]],
     repeatPassword: ['', [Validators.required, passwordEqualsValidator]],
     country: ['', Validators.required],
     city: ['', Validators.required],
@@ -56,6 +77,29 @@ export class SignUpComponent {
   }
 
   countryCodes = COUNTRY_CODES;
+
+  private passwordSignal = toSignal(this.formGroup.controls.password.valueChanges);
+  passwordCriteriaPassed: Signal<PasswordCriteriaModel> = computed(() => {
+    const password = this.passwordSignal();
+    return {
+      length: password ? password.length > 5 : false,
+      uppercase: password ? password.split('').some(l => l === l.toUpperCase()) : false,
+      lowercase: password ? password.split('').some(l => l === l.toLowerCase()) : false,
+      number: new RegExp(/\d/).test(password ?? ''),
+      char: new RegExp(/[#?!@$%^&*-]/).test(password ?? ''),
+    }
+  });
+  passwordCriteriaIcons: Signal<{ [key in keyof PasswordCriteriaModel]: string }> = computed(() => {
+    const criteria = this.passwordCriteriaPassed();
+    const icons = ['@tui.check', '@tui.dot'];
+    return {
+      length: criteria.length ? icons[0]: icons[1],
+      uppercase: criteria.uppercase ? icons[0]: icons[1],
+      lowercase: criteria.lowercase ? icons[0]: icons[1],
+      number: criteria.number ? icons[0]: icons[1],
+      char: criteria.char ? icons[0]: icons[1],
+    }
+  });
 
   toGender(value: any): Gender {
     return value as Gender;
