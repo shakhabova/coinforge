ec660ffdec566952c6446dabc459fea620f85f66
Алина Шахабова <alina.shakhabova@yandex.ru>
2024-09-17 16:58:22 +0300

    work in progress

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TotalBalanceComponent } from './total-balance/total-balance.component';
 import { WalletsComponent } from './wallets/wallets.component';
+import { MarketInfoComponent } from './market-info/market-info.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,7 +11,9 @@ import { WalletsComponent } from './wallets/wallets.component';
   imports: [
     CommonModule,
     TotalBalanceComponent,
-    WalletsComponent
+    WalletsComponent,
+    MarketInfoComponent,
+    TransactionsComponent
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
---
+++ src/app/components/dashboard/market-info/market-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-market-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './market-info.component.html',
+  styleUrl: './market-info.component.css'
+})
+export class MarketInfoComponent {
+
+}
