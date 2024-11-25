ebd5b6edc8ec53efaeb2e44e59774ea4f4b7adad
Алина Шахабова <alina.shakhabova@yandex.ru>
2024-11-25 14:42:11 +0300

    transactions ready

---
+++ src/app/components/dashboard/transactions/transaction-item/transaction-item.component.ts
@@ -0,0 +1,44 @@
+import { DatePipe } from '@angular/common';
+import { Component, computed, input } from '@angular/core';
+import { TuiIcon } from '@taiga-ui/core';
+import { TransactionDto } from 'services/transactions.service';
+
+const ICON_TYPE_MAP: Record<TransactionDto['type'], string> = {
+  IN: '@tui.chevron-down',
+  F2C: '@tui.chevron-down',
+  C2C: '@tui.chevron-down',
+
+  OUT: '@tui.chevron-up',
+  C2F: '@tui.chevron-up',
+};
+
+const STATUSES_MAP: Record<TransactionDto['oprStatus'], string> = {
+  CONFIRMED: 'Confirmed',
+  REFUNDED: 'Refunded',
+  REJECTED: 'Rejected',
+};
+
+@Component({
+  selector: 'app-transaction-item',
+  standalone: true,
+  imports: [
+    TuiIcon,
+    DatePipe,
+  ],
+  templateUrl: './transaction-item.component.html',
+  styleUrl: './transaction-item.component.css'
+})
+export class TransactionItemComponent {
+  transaction = input.required<TransactionDto>();
+
+  typeIcon = computed(() => ICON_TYPE_MAP[this.transaction().type]);
+  oprStatus = computed(() => STATUSES_MAP[this.transaction().oprStatus]);
+
+  isPositiveOpr = computed(() => ['IN', 'F2C', 'C2C'].includes(this.transaction().type));
+
+  private amountSum = computed(() => this.isPositiveOpr() ? this.transaction().amount : this.transaction().amountInSenderCurrency);
+  private amountCurr = computed(() => this.isPositiveOpr() ? this.transaction().currencyTo : this.transaction().currencyFrom);
+  amount = computed(() => `${this.isPositiveOpr() ? '+' : '-'} ${this.amountSum()} ${this.amountCurr()}`);
+}
+
+
---
+++ src/app/services/transactions.service.ts
@@ -0,0 +1,184 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ConfigService } from './config.service';
+import { Observable, of } from 'rxjs';
+import { PageableParams, PageableResponse } from 'models/pageable.model';
+
+export interface TransactionDto {
+  id: string;
+  fromOprAddress: string;
+  fromTrxAddress: string;
+  toOprAddress: unknown;
+  toTrxAddress: string;
+  customerId: string;
+  receiverCustomerId: unknown;
+  institutionId: number;
+  amount: string;
+  amountInSenderCurrency: string;
+  creditAmount: string;
+  debitAmount: string;
+  totalCommissionAmount: string;
+  exchangeCommissionAmount: string;
+  transactionCommissionAmount: string;
+  maxSlippage: unknown;
+  slippageCommissionAmount: unknown;
+  currencyFrom: string;
+  currencyTo: string;
+  exchangeRate: number;
+  requestedRate: unknown;
+  transactionId: number;
+  transactionFee: unknown;
+  gasPrice: unknown;
+  transactionHash: string;
+  trxStatus: string;
+  statusDescription: unknown;
+  cryptocurrency: string;
+  oprStatus: 'REJECTED' | 'CONFIRMED' | 'REFUNDED';
+  type: 'IN' | 'OUT' | 'C2F' | 'F2C' | 'C2C';
+  category: string;
+  commissionIsTransferred: unknown;
+  provider: unknown;
+  amountDirection: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TransactionsService {
+  private httpClient = inject(HttpClient);
+  private configService = inject(ConfigService);
+
+  getTransactions(
+    params: PageableParams
+  ): Observable<PageableResponse<TransactionDto>> {
+    return of({
+      data: [],
+      pageNumber: 0,
+      pageSize: 10,
+      totalElements: 1,
+    });
+    // return this.httpClient.get<PageableResponse<TransactionDto>>(`${this.configService.serverUrl}/v1/bff-custody/transactions`, { params: params as HttpParams });
+  }
+}
+
+const mockData = [
+        {
+          id: 'OUT631',
+          fromOprAddress: 'GUA_ARB_9f1de16c-63be-42ca-8a25-7eec09c6821d',
+          fromTrxAddress: '0x4F5E892F041f23bD56DAd2B8D957cFA926B92B6B',
+          toOprAddress: null,
+          toTrxAddress: '0x62886a46C1b71BA8998AF57c5e7Fc7b0a44a877C',
+          customerId: '00010004',
+          receiverCustomerId: null,
+          institutionId: 1,
+          amount: '1',
+          amountInSenderCurrency: '1',
+          creditAmount: '1',
+          debitAmount: '1',
+          totalCommissionAmount: '0',
+          exchangeCommissionAmount: '0',
+          transactionCommissionAmount: '0',
+          maxSlippage: null,
+          slippageCommissionAmount: null,
+          currencyFrom: 'ARB',
+          currencyTo: 'ARB',
+          exchangeRate: 1,
+          requestedRate: null,
+          transactionId: 505,
+          transactionFee: null,
+          gasPrice: null,
+          transactionHash:
+            '0x173e10397ac8dfccc460c066ac4d1fba04830f38ccc82b8d1720470eab942708',
+          trxStatus: 'CONFIRMED',
+          statusDescription: null,
+          cryptocurrency: 'ARB',
+          oprStatus: 'CONFIRMED',
+          type: 'OUT',
+          category: 'TRANSACTION',
+          commissionIsTransferred: null,
+          provider: null,
+          amountDirection: 'currencyFrom',
+          createdAt: '2024-11-21T09:16:49.699945',
+          updatedAt: '2024-10-04T09:17:24.828483',
+        },
+        {
+          id: 'OUT632',
+          fromOprAddress: 'GUA_ARB_9f1de16c-63be-42ca-8a25-7eec09c6821d',
+          fromTrxAddress: '0x4F5E892F041f23bD56DAd2B8D957cFA926B92B6B',
+          toOprAddress: null,
+          toTrxAddress: '0x62886a46C1b71BA8998AF57c5e7Fc7b0a44a877C',
+          customerId: '00010004',
+          receiverCustomerId: null,
+          institutionId: 1,
+          amount: '1',
+          amountInSenderCurrency: '1',
+          creditAmount: '1',
+          debitAmount: '1',
+          totalCommissionAmount: '0',
+          exchangeCommissionAmount: '0',
+          transactionCommissionAmount: '0',
+          maxSlippage: null,
+          slippageCommissionAmount: null,
+          currencyFrom: 'ARB',
+          currencyTo: 'ARB',
+          exchangeRate: 1,
+          requestedRate: null,
+          transactionId: 505,
+          transactionFee: null,
+          gasPrice: null,
+          transactionHash:
+            '0x173e10397ac8dfccc460c066ac4d1fba04830f38ccc82b8d1720470eab942708',
+          trxStatus: 'CONFIRMED',
+          statusDescription: null,
+          cryptocurrency: 'ARB',
+          oprStatus: 'CONFIRMED',
+          type: 'OUT',
+          category: 'TRANSACTION',
+          commissionIsTransferred: null,
+          provider: null,
+          amountDirection: 'currencyFrom',
+          createdAt: '2024-11-22T09:16:49.699945',
+          updatedAt: '2024-10-04T09:17:24.828483',
+        },
+        {
+          id: 'OUT633',
+          fromOprAddress: 'GUA_ARB_9f1de16c-63be-42ca-8a25-7eec09c6821d',
+          fromTrxAddress: '0x4F5E892F041f23bD56DAd2B8D957cFA926B92B6B',
+          toOprAddress: null,
+          toTrxAddress: '0x62886a46C1b71BA8998AF57c5e7Fc7b0a44a877C',
+          customerId: '00010004',
+          receiverCustomerId: null,
+          institutionId: 1,
+          amount: '1',
+          amountInSenderCurrency: '1',
+          creditAmount: '1',
+          debitAmount: '1',
+          totalCommissionAmount: '0',
+          exchangeCommissionAmount: '0',
+          transactionCommissionAmount: '0',
+          maxSlippage: null,
+          slippageCommissionAmount: null,
+          currencyFrom: 'ARB',
+          currencyTo: 'ARB',
+          exchangeRate: 1,
+          requestedRate: null,
+          transactionId: 505,
+          transactionFee: null,
+          gasPrice: null,
+          transactionHash:
+            '0x173e10397ac8dfccc460c066ac4d1fba04830f38ccc82b8d1720470eab942708',
+          trxStatus: 'CONFIRMED',
+          statusDescription: null,
+          cryptocurrency: 'ARB',
+          oprStatus: 'CONFIRMED',
+          type: 'OUT',
+          category: 'TRANSACTION',
+          commissionIsTransferred: null,
+          provider: null,
+          amountDirection: 'currencyFrom',
+          createdAt: '2024-11-22T09:16:49.699945',
+          updatedAt: '2024-10-04T09:17:24.828483',
+        },
+      ]
