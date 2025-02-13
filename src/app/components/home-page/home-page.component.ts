191f6ffa74bb6518b11ec7b41a30e93c1feb4978
Алина Шахабова <alina.shakhabova@yandex.ru>
2025-02-13 17:28:57 +0300

    services-card component added

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MissionComponent } from './mission/mission.component';
+import { ServicesCardComponent } from './services-card/services-card.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
-    MissionComponent
+    MissionComponent,
+    ServicesCardComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
---
+++ src/app/components/home-page/services-card/services-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './services-card.component.html',
+  styleUrl: './services-card.component.scss'
+})
+export class ServicesCardComponent {
+
+}
