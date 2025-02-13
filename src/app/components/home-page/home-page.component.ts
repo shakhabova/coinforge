6795bc302e489d28d8ab23a583bf48b7f80564ef
Алина Шахабова <alina.shakhabova@yandex.ru>
2025-02-13 17:45:10 +0300

    contact-us component added

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { MissionComponent } from './mission/mission.component';
 import { ServicesCardComponent } from './services-card/services-card.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
     MissionComponent,
-    ServicesCardComponent
+    ServicesCardComponent,
+    ContactUsComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
