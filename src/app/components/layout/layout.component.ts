e1f96589bfad716998a021ee02ca21134ca7dccb
Алина Шахабова <alina.shakhabova@yandex.ru>
2025-02-25 17:24:14 +0300

    footer component added

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -3,13 +3,15 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthInterceptor } from 'interceptors/auth-interceptor.service';
 import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
     RouterModule,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
 ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
