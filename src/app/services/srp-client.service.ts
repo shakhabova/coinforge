6b99d5b6932a8d57ed216b24480661bc25a18fa8
Алина Шахабова <alina.shakhabova@yandex.ru>
2025-03-24 10:02:34 +0300

    getting rig of srp server

--- src/app/services/srp-client.service.ts
+++ src/app/services/srp-client.service.ts
@@ -1,22 +1,11 @@
 import { Injectable } from '@angular/core';
 import thinbusSRP from 'thinbus-srp/browser.js';
-import thinbusSRPServer from 'thinbus-srp/server.js';
 import { environment } from '../../environment/environment';
-import { Buffer } from 'buffer';
-
-// @ts-ignore
-window.Buffer = Buffer;
 
 @Injectable({
 	providedIn: 'root',
 })
 export class SrpClientService {
-	constructor() {
-		// @ts-ignore
-		window.server = thinbusSRPServer;
-		// @ts-ignore
-		window.client = thinbusSRP;
-	}
 	public srpClient() {
 		const SrpClientConstructor = thinbusSRP(
 			environment.srp6.N_base10,
