b1030620c7e1ea852e7dae448fde07ed0f0ef189
Алина Шахабова <alina.shakhabova@yandex.ru>
2025-05-22 23:03:07 +0300

    contact us form fixed

--- src/app/components/home-page/contact-page/contact-page.component.ts
+++ src/app/components/home-page/contact-page/contact-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-contact-page',
-	imports: [],
+	imports: [FormsModule],
 	templateUrl: './contact-page.component.html',
 	styleUrl: './contact-page.component.scss',
 })
-export class ContactPageComponent {}
+export class ContactPageComponent {
+	comment = '';
+}
